c4ba4c7d949d0c21f2f356ac0dfa87035683c844
Omar Valdez <omarantoniovaldezf2@gmail.com>
2025-09-25 21:56:57 -0700

    feat(cli): support decimal percentages

--- src/main.rs
+++ src/main.rs
@@ -7,27 +7,39 @@ use std::process::ExitCode;
 use crate::percent::Percent;
 
 mod percent {
-    #[derive(Debug, PartialEq)]
-    pub struct Percent(u8);
+    #[derive(Debug, Clone, PartialEq)]
+    pub struct Percent(f32);
 
     impl Percent {
-        pub fn new(value: u8) -> Self {
-            Self(value.max(100))
+        pub fn new(p: f32) -> Option<Self> {
+            if p.is_finite() && (0.0..=100.0).contains(&p) { Some(Self(p)) } else { None }
         }
 
-        pub fn get(&self) -> u8 {
+        pub fn get(&self) -> f32 {
             self.0
+        }
+    }
+
+    pub fn clap_parser(s: &str) -> Result<Percent, String> {
+        let percent: f32 = s.parse().map_err(|_| format!("`{s}` is not a percentage"))?;
+        match Percent::new(percent) {
+            Some(p) => Ok(p),
+            None => Err(format!("`{s}` is not a percentage between 0 and 100")),
         }
     }
 
     #[test]
     fn test_percent() {
-        use std::u8;
-        assert_eq!(Percent::new(0), Percent(0));
-        assert_eq!(Percent::new(15), Percent(15));
-        assert_eq!(Percent::new(100), Percent(100));
-        assert_eq!(Percent::new(101), Percent(100));
-        assert_eq!(Percent::new(u8::MAX), Percent(100));
+        assert_eq!(Percent::new(0.0), Some(Percent(0.0)));
+        assert_eq!(Percent::new(15.0), Some(Percent(15.0)));
+        assert_eq!(Percent::new(100.0), Some(Percent(100.0)));
+        assert_eq!(Percent::new(-1.0), None);
+        assert_eq!(Percent::new(101.0), None);
+        assert_eq!(Percent::new(f32::MIN), None);
+        assert_eq!(Percent::new(f32::MAX), None);
+        assert_eq!(Percent::new(f32::NAN), None);
+        assert_eq!(Percent::new(f32::INFINITY), None);
+        assert_eq!(Percent::new(f32::NEG_INFINITY), None);
     }
 }
 
@@ -80,11 +92,7 @@ impl Device {
 
                 assert!(brightness <= max_brightness);
 
-                Device {
-                    name,
-                    brightness,
-                    max_brightness,
-                }
+                Device { name, brightness, max_brightness }
             })
             .collect::<Vec<_>>();
 
@@ -99,23 +107,23 @@ impl Device {
 /// [perception]: https://konradstrack.ninja/blog/changing-screen-brightness-in-accordance-with-human-perception/
 pub fn brightness_from_percent(percent: &Percent, max_brightness: Brightness) -> Brightness {
     let percent = percent.get();
-    if percent == 0 || max_brightness == 0 {
+    if percent == 0.0 || max_brightness == 0 {
         return 0;
     }
-    let exp = (percent as f32 / 100.0) * (max_brightness as f32).log10();
+    let exp = (percent / 100.0) * (max_brightness as f32).log10();
     (10_f32).powf(exp).round() as Brightness // Float to integer is a saturated cast
 }
 
 /// Inverse of `brightness_from_percent`.
 pub fn brightness_to_percent(brightness: Brightness, max_brightness: Brightness) -> Percent {
     if brightness == 0 {
-        return Percent::new(0);
+        return Percent::new(0.0).unwrap();
     }
     if max_brightness <= 1 {
-        return Percent::new(if brightness <= max_brightness { 0 } else { 100 });
+        return Percent::new(if brightness <= max_brightness { 0.0 } else { 100.0 }).unwrap();
     }
     let percent = (brightness as f32).log(max_brightness as f32) * 100.0;
-    Percent::new(percent.round() as u8)
+    Percent::new(percent).expect("percent calculcation to always give a valid value")
 }
 
 fn update_brightness<F>(args: &SubArgs, calc_brightness: F) -> ExitCode
@@ -128,8 +136,7 @@ where
         return ExitCode::FAILURE;
     };
 
-    let percent = Percent::new(args.percent);
-    let brightness = brightness_from_percent(&percent, device.max_brightness);
+    let brightness = brightness_from_percent(&args.percent, device.max_brightness);
     let total_brightness = calc_brightness(device, brightness);
 
     match device.set_brightness(total_brightness) {
@@ -159,26 +166,22 @@ enum Command {
 
 #[derive(Debug, Args)]
 struct SubArgs {
-    #[arg(value_parser = clap::value_parser!(u8).range(0..=100))]
-    percent: u8,
+    #[arg(value_parser = percent::clap_parser)]
+    percent: Percent,
 }
 
 fn main() -> ExitCode {
     let cli = Cli::parse();
 
     match cli.command {
-        Command::Add(args) => update_brightness(&args, |dev, brightness| {
-            dev.brightness.saturating_add(brightness)
-        }),
-        Command::Sub(args) => update_brightness(&args, |dev, brihtness| {
-            dev.brightness.saturating_sub(brihtness)
-        }),
+        Command::Add(args) => update_brightness(&args, |dev, b| dev.brightness.saturating_add(b)),
+        Command::Sub(args) => update_brightness(&args, |dev, b| dev.brightness.saturating_sub(b)),
         Command::Set(args) => update_brightness(&args, |_, brightness| brightness),
         Command::Get => {
             let devices = Device::get_all();
             if let Some(device) = devices.first() {
-                let percent = brightness_to_percent(device.brightness, device.max_brightness);
-                println!("{}", percent.get());
+                let percent = brightness_to_percent(device.brightness, device.max_brightness).get();
+                println!("{percent:.2}");
             } else {
                 eprintln!("no devices found");
             };
@@ -212,37 +215,40 @@ mod test {
 
     #[test]
     fn test_brightness_from_percent() {
-        assert_eq!(brightness_from_percent(&Percent::new(0), 100), 0);
-        assert_eq!(brightness_from_percent(&Percent::new(10), 100), 2);
-        assert_eq!(brightness_from_percent(&Percent::new(20), 100), 3);
-        assert_eq!(brightness_from_percent(&Percent::new(30), 100), 4);
-        assert_eq!(brightness_from_percent(&Percent::new(40), 100), 6);
-        assert_eq!(brightness_from_percent(&Percent::new(50), 100), 10);
-        assert_eq!(brightness_from_percent(&Percent::new(60), 100), 16);
-        assert_eq!(brightness_from_percent(&Percent::new(70), 100), 25);
-        assert_eq!(brightness_from_percent(&Percent::new(80), 100), 40);
-        assert_eq!(brightness_from_percent(&Percent::new(90), 100), 63);
-        assert_eq!(brightness_from_percent(&Percent::new(95), 100), 79);
-        assert_eq!(brightness_from_percent(&Percent::new(99), 100), 95);
-        assert_eq!(brightness_from_percent(&Percent::new(100), 100), 100);
-        assert_eq!(brightness_from_percent(&Percent::new(100), 12345), 12345);
+        assert_eq!(brightness_from_percent(&Percent::new(0.0).unwrap(), 100), 0);
+        assert_eq!(brightness_from_percent(&Percent::new(10.0).unwrap(), 100), 2);
+        assert_eq!(brightness_from_percent(&Percent::new(20.0).unwrap(), 100), 3);
+        assert_eq!(brightness_from_percent(&Percent::new(30.0).unwrap(), 100), 4);
+        assert_eq!(brightness_from_percent(&Percent::new(40.0).unwrap(), 100), 6);
+        assert_eq!(brightness_from_percent(&Percent::new(50.0).unwrap(), 100), 10);
+        assert_eq!(brightness_from_percent(&Percent::new(60.0).unwrap(), 100), 16);
+        assert_eq!(brightness_from_percent(&Percent::new(70.0).unwrap(), 100), 25);
+        assert_eq!(brightness_from_percent(&Percent::new(80.0).unwrap(), 100), 40);
+        assert_eq!(brightness_from_percent(&Percent::new(90.0).unwrap(), 100), 63);
+        assert_eq!(brightness_from_percent(&Percent::new(95.0).unwrap(), 100), 79);
+        assert_eq!(brightness_from_percent(&Percent::new(99.0).unwrap(), 100), 95);
+        assert_eq!(brightness_from_percent(&Percent::new(100.0).unwrap(), 100), 100);
+        assert_eq!(brightness_from_percent(&Percent::new(100.0).unwrap(), 12345), 12345);
     }
 
     #[test]
     fn test_brightness_to_percent() {
-        assert_eq!(brightness_to_percent(0, 100), Percent::new(0));
-        assert_eq!(brightness_to_percent(2, 100), Percent::new(15));
-        assert_eq!(brightness_to_percent(3, 100), Percent::new(24));
-        assert_eq!(brightness_to_percent(4, 100), Percent::new(30));
-        assert_eq!(brightness_to_percent(6, 100), Percent::new(39));
-        assert_eq!(brightness_to_percent(10, 100), Percent::new(50));
-        assert_eq!(brightness_to_percent(16, 100), Percent::new(60));
-        assert_eq!(brightness_to_percent(25, 100), Percent::new(70));
-        assert_eq!(brightness_to_percent(40, 100), Percent::new(80));
-        assert_eq!(brightness_to_percent(63, 100), Percent::new(90));
-        assert_eq!(brightness_to_percent(79, 100), Percent::new(95));
-        assert_eq!(brightness_to_percent(95, 100), Percent::new(99));
-        assert_eq!(brightness_to_percent(100, 100), Percent::new(100));
-        assert_eq!(brightness_to_percent(12345, 12345), Percent::new(100));
+        use assert_float_eq::assert_float_absolute_eq;
+
+        let ep = 0.01; // epsilon
+        assert_float_absolute_eq!(brightness_to_percent(0, 100).get(), 0.0, ep);
+        assert_float_absolute_eq!(brightness_to_percent(2, 100).get(), 15.05, ep);
+        assert_float_absolute_eq!(brightness_to_percent(3, 100).get(), 23.86, ep);
+        assert_float_absolute_eq!(brightness_to_percent(4, 100).get(), 30.10, ep);
+        assert_float_absolute_eq!(brightness_to_percent(6, 100).get(), 38.91, ep);
+        assert_float_absolute_eq!(brightness_to_percent(10, 100).get(), 50.0, ep);
+        assert_float_absolute_eq!(brightness_to_percent(16, 100).get(), 60.21, ep);
+        assert_float_absolute_eq!(brightness_to_percent(25, 100).get(), 69.89, ep);
+        assert_float_absolute_eq!(brightness_to_percent(40, 100).get(), 80.10, ep);
+        assert_float_absolute_eq!(brightness_to_percent(63, 100).get(), 89.96, ep);
+        assert_float_absolute_eq!(brightness_to_percent(79, 100).get(), 94.88, ep);
+        assert_float_absolute_eq!(brightness_to_percent(95, 100).get(), 98.88, ep);
+        assert_float_absolute_eq!(brightness_to_percent(100, 100).get(), 100.0, ep);
+        assert_float_absolute_eq!(brightness_to_percent(12345, 12345).get(), 100.0, ep);
     }
 }
